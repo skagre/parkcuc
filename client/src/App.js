a06749c9a74dcb4314b807c65b6c1c2d081ffc6c
unknown <skagre4@gmail.com>
2020-11-18 20:03:35 +0700

    18-11-2020

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,27 @@ import {withStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import MenuList from '@material-ui/core/MenuList';
 import MenuItem from '@material-ui/core/MenuItem';
-import { Grid } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 import GroupAddIcon from '@material-ui/icons/GroupAdd';
 import PeopleIcon from '@material-ui/icons/People';
 import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver';
 import SettingsIcon from '@material-ui/icons/Settings';
 
 
-export default function CustomizedMenus() {
+const App = props => {
     return (
-        <Grid className="b__container" container>
-            <MainNav />
+        <Grid container>
+            <Grid item lg={3} md={3} style={{display: 'flex'}}>
+                <MainNav />
+            </Grid>
+            <Grid item lg={6} md={9}>
+
+            </Grid>
+            <Grid item lg={3}>
+
+            </Grid>
         </Grid>
     );
 }
+
+export default App
--- client/src/components/LeftSidebar/MainNav.js
+++ client/src/components/LeftSidebar/MainNav.js
@@ -1,21 +1,153 @@
-import React from 'react'
-import MenuList from '@material-ui/core/MenuList'
-import MenuItem from '@material-ui/core/MenuItem'
-import GroupAddIcon from '@material-ui/icons/GroupAdd'
-import PeopleIcon from '@material-ui/icons/People'
-import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver'
-import SettingsIcon from '@material-ui/icons/Settings'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+import Avatar from '@material-ui/core/Avatar'
+import Tooltip from '@material-ui/core/Tooltip'
+import Divider from '@material-ui/core/Divider'
+
+import SettingsTwoToneIcon from '@material-ui/icons/SettingsTwoTone'
+import NotificationsTwoToneIcon from '@material-ui/icons/NotificationsTwoTone'
+import PeopleAltTwoToneIcon from '@material-ui/icons/PeopleAltTwoTone'
+import Brightness4TwoToneIcon from '@material-ui/icons/Brightness4TwoTone'
+import RecordVoiceOverTwoToneIcon from '@material-ui/icons/RecordVoiceOverTwoTone'
+import SmsTwoToneIcon from '@material-ui/icons/SmsTwoTone'
+
+import { orange, pink, green, blue } from "@material-ui/core/colors"
 
 
-import './MainNav.css'
-const MainNav = props => {
+const TabPanel = props => {
+    const { children, value, index, ...other } = props
+
     return (
-        <MenuList className="b__main-nav">
-            <MenuItem><GroupAddIcon /></MenuItem>
-            <MenuItem><PeopleIcon /></MenuItem>
-            <MenuItem><RecordVoiceOverIcon /></MenuItem>
-            <MenuItem><SettingsIcon /></MenuItem>
-        </MenuList>
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+        {value === index && (
+            <Box p={3}>
+            <Typography>{children}</Typography>
+            </Box>
+        )}
+        </div>
+    )
+}
+  
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+}
+  
+const a11yProps = index => {
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    }
+}
+  
+const theme = createMuiTheme({
+    overrides: {
+        MuiTabs: {
+            root: {
+                width: 80,
+                height: '100vh'
+            }
+        },
+        MuiTab: {
+            root: {
+                minWidth: 0,
+                padding: '18px 12px',
+                '@media (min-width: 600px)': {
+                    minWidth: 0
+                }
+            }
+        },
+        MuiDivider: {
+            root: {
+                backgroundColor: blue[900]
+            }
+        },
+        PrivateTabIndicator: {
+            root: {
+                backgroundColor: blue[900]
+            }
+        },
+        MuiSvgIcon: {
+            root: {
+                fontSize: 28,
+                color: blue[900],
+            }
+        }
+    }
+})
+
+const MainNav = props => {
+
+    const [value, setValue] = useState(0)
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                aria-label="Main Navigation"
+                indicatorColor="primary"
+                centered
+            >
+                <Tooltip title="Profile" placement="right" arrow>
+                    <Tab icon={<Avatar>B</Avatar>} {...a11yProps(0)} />
+                </Tooltip>
+                <Tooltip title="Friends Online" placement="right" arrow>
+                    <Tab icon={<SmsTwoToneIcon />} {...a11yProps(1)} />
+                </Tooltip>
+                <Tooltip title="Friends Online" placement="right" arrow>
+                    <Tab icon={<RecordVoiceOverTwoToneIcon />} {...a11yProps(1)} />
+                </Tooltip>
+                <Tooltip title="Friends" placement="right" arrow>
+                    <Tab icon={<PeopleAltTwoToneIcon />} {...a11yProps(2)} />
+                </Tooltip>
+                <Tooltip title="Notification" placement="right" arrow>
+                    <Tab icon={<NotificationsTwoToneIcon />} {...a11yProps(3)} />
+                </Tooltip>
+                <Tooltip title="Dark Mode" placement="right" arrow>
+                    <Tab icon={<Brightness4TwoToneIcon />} {...a11yProps(4)} />
+                </Tooltip>
+                <Tooltip title="Setting" placement="right" arrow>
+                    <Tab icon={<SettingsTwoToneIcon />} {...a11yProps(5)} />
+                </Tooltip>
+            </Tabs>
+            <TabPanel value={value} index={0}>
+                Profile
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Item Two asd
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                Item Three
+            </TabPanel>
+            <TabPanel value={value} index={3}>
+                Item Four
+            </TabPanel>
+            <TabPanel value={value} index={4}>
+                Item Five
+            </TabPanel>
+            <TabPanel value={value} index={5}>
+                Item Six
+            </TabPanel>
+        </ThemeProvider>
     )
 }
 
